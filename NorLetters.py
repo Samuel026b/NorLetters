f36ce55a83566ce3951d46635526a2ea2067ec5a
Samuel Brox <Samuel.Brox026@gmail.com>
2023-04-22 18:10:27 +0200

    some minor changes

--- NorLetters.py
+++ NorLetters.py
@@ -1,3 +1,9 @@
+
+import time
+import sys
+
+user_agent_header = {'User-Agent': 'NorLetters - https://github.com/Samuel026B/NorLetters - samuel.brox026@gmail.com'}
+
 class NorLetters:
   def __init__(self, letter, count_letters):
     self.letter = letter
@@ -9,7 +15,7 @@ def sort_descending(arr):
         arr[i], arr[j] = arr[j], arr[i]
   return arr
 import requests
-import pyperclip
+#import pyperclip
 dcheck = []
 def count_chars(text):
     """Count the frequency of each character in the given text."""
@@ -18,35 +24,45 @@ def count_chars(text):
         freq[char] = freq.get(char, 0) + 1
     return freq
 mylist = []
-while True:
-  NUMBERofRUNS = int(input("> "))
-  if NUMBERofRUNS > 100:
-    raise ValueError("too many runs - your IP adress may be blocked from Wikipedia.")
-  else:
-    break
+if len(sys.argv)>1:
+  NUMBERofRUNS=int(sys.argv[1])
+else:
+  while True:
+    NUMBERofRUNS = int(input("> "))
+    if NUMBERofRUNS > 100:
+      raise ValueError("too many runs - your IP adress may be blocked from Wikipedia.")
+    else:
+      break
 pagestr = ""
 for WhatEver in range(NUMBERofRUNS):
-  r = requests.get("https://no.wikipedia.org/wiki/Spesial:Tilfeldig", allow_redirects=False)
-  random_page_url = r.headers['location']
-  pagestr += f"{random_page_url} "
-  print(f"{WhatEver + 1}) {random_page_url}")
-  random_page_title = random_page_url.split('/')[-1]
-  text_json = requests.get(f"https://no.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles={random_page_title}&rvslots=main").json()
-  pages_data = text_json['query']['pages'].values()
-  for page_data in pages_data:
-      for revision_data in page_data['revisions']:
-          page_text = revision_data['slots']['main']['*']
-          freq = count_chars(page_text)
-          for char, count in freq.items():
-              if char.upper() in dcheck:
-                mylist[dcheck.index(char.upper())].number += count
-              elif (char.upper() >= "A" and char.upper() <= "Z") or char.upper() in ["Æ", "Ø", "Å"]:
-                mylist.append(NorLetters(char.upper(), count_letters=count))
-                dcheck.append(char.upper())
+  try:
+    time.sleep(4)
+    r = requests.get("https://no.wikipedia.org/wiki/Spesial:Tilfeldig", allow_redirects=False, headers=user_agent_header)
+    random_page_url = r.headers['location']
+    pagestr += f"{random_page_url} "
+    print(f"{WhatEver + 1}) {random_page_url}")
+    random_page_title = random_page_url.split('/')[-1]
+    text_json = requests.get(f"https://no.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles={random_page_title}&rvslots=main", headers=user_agent_header).json()
+    pages_data = text_json['query']['pages'].values()
+    for page_data in pages_data:
+        if not 'revisions' in page_data:
+            print(f"foobar in {random_page_title}")
+            continue
+        for revision_data in page_data['revisions']:
+            page_text = revision_data['slots']['main']['*']
+            freq = count_chars(page_text)
+            for char, count in freq.items():
+                if char.upper() in dcheck:
+                  mylist[dcheck.index(char.upper())].number += count
+                elif ((char.upper() >= "A" and char.upper() <= "Z") or char.upper() in ["Æ", "Ø", "Å"]) and not char.upper() == "SS":
+                  mylist.append(NorLetters(char.upper(), count_letters=count))
+                  dcheck.append(char.upper())
+  except KeyboardInterrupt:
+    print(" KeyboardInterrupt. Here are the result of the articles that were counted:") # Please correct me if my grammar is wrong.
+    break
 total_occurences = 0
 for item in mylist:
   total_occurences += item.number
 mylist = sort_descending(mylist)
 for item in mylist:
   print(f'"{item.letter}": {item.number / total_occurences * 100:.2f}% ({item.number})')
-pyperclip.copy(pagestr)
